32afb1135b67f09caf00281acb71ec478f5081c2
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-17 22:33:51 +0300

    example9

---
+++ example9/Program.cs
@@ -0,0 +1,14 @@
+﻿//9 Напишите программу, которая выводит случайное число из отрезка
+// [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+int num = new Random().Next(10,100);
+Console.WriteLine($"Num = {num}");
+int num1 = num / 10;
+int num2 = num % 10;
+int max = num2;
+if(num1 > num2){
+    max = num1;
+}
+Console.WriteLine($"Max = {max}");
